0186e3a6a8864321bc828c8a7f617e02249cd1ee
Yan Rodrigues <yanrdgaz@gmail.com>
2023-07-18 11:25:20 -0300

    Ex 001 and Ex 002 completed.

---
+++ ex001.py
@@ -0,0 +1,8 @@
+# Crie um programa que escreva "Olá, Mundo!":
+
+print('Olá, Mundo!')
+
+# Alternativa:
+
+msg = 'Olá, Mundo!'
+print(msg)
---
+++ ex002.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas:
+
+name = input('Digite seu nome:')
+print('É um prazer te conhecer, {}!'.format(name))
