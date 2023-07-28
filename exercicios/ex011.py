dcb7c35a38c53de340bfe6b1ab034dbd6b51f9eb
Yan Rodrigues <yanrdgaz@gmail.com>
2023-07-28 15:24:47 -0300

    Atualização no ex011.py. Troca de int por float.

--- exercicios/ex011.py
+++ exercicios/ex011.py
@@ -1,9 +1,9 @@
 # Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta
 # necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m².
 
-largura = int(input('Digite a largura da parede em metros: '))
-altura = int(input('Digite a altura da parede em metros: '))
+largura = float(input('Digite a largura da parede em metros: '))
+altura = float(input('Digite a altura da parede em metros: '))
 area = largura * altura
 
 print(f'Considerando a largura e altura mencionadas, a área desta parede é {area}m².\nConsiderando que 1L de tinta '
-      f'pinta 2m², será necessário {int(area/2)}L para pintar a parede em questão.')
+      f'pinta 2m², será necessário {(area/2)}L para pintar a parede em questão.')
