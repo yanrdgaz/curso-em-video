7b0ba53a67731e0c0adc71f5981f6148deba9637
Yan Rodrigues <yanrdgaz@gmail.com>
2023-07-28 15:19:56 -0300

    Atualização do ex010.py. Adicionado 'R$' ao final do input.

--- exercicios/ex010.py
+++ exercicios/ex010.py
@@ -1,5 +1,5 @@
 # Crie um programa que leia o quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 # Considere US$ 1.00 = R$ 3.27
 
-n = float(input('Digite o valor que possui em reais: '))
+n = float(input('Digite o valor que possui em reais: R$ '))
 print(f'R$ {n:.2f} pode comprar o equivalente aproximado de US$ {n/3.27:.2f}.')
