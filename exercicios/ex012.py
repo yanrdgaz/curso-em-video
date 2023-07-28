8bc5a9ee96311f786a3bfff744bc6a4f95933b14
Yan Rodrigues <yanrdgaz@gmail.com>
2023-07-28 11:26:16 -0300

    Exercícios 011 ao 013 completados.

---
+++ exercicios/ex012.py
@@ -0,0 +1,4 @@
+# Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+n = float(input('Digite o valor do produto em reais (R$): '))
+print(f'Aplicando 5% de desconto, o valor deste produto se torna R$ {n*0.95:.2f}.')
---
+++ exercicios/ex013.py
@@ -0,0 +1,5 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+n = float(input('Digite o valor de seu salário atual em reais (R$): '))
+
+print(f'Parabéns! Com o aumento de 15%, seu salário se torna R$ {n*1.15:.2f}.')
