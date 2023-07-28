590ff4bd7358c0e682330fc004c60b0033b4ec1b
Yan Rodrigues <yanrdgaz@gmail.com>
2023-07-28 14:47:24 -0300

    Adicionado forma com ponto flutuante no ex007.py.

--- exercicios/ex007.py
+++ exercicios/ex007.py
@@ -5,6 +5,11 @@ n1 = int(input('Digite a primeira nota: '))
 n2 = int(input('Digite a segunda nota: '))
 print(f'A média das notas é {(n1+n2)/2}.')
 
+# Forma com ponto flutuante:
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print(f'A média das notas é {(n1+n2)/2:.1f}.')
+
 # Adaptação:
 n1 = float(input('Digite a primeira nota (com uma casa decimal): '))
 n2 = float(input('Digite a segunda nota (com uma casa decimal): '))
